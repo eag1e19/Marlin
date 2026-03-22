7b2e079040298ddb563b0c04cdfb644847c16fc3
eag1e19 <tdorn6@gmail.com>
2026-03-22 15:05:36 -0400

    Configured X_MIN end stop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1269,7 +1269,7 @@
  * Endstop "Hit" State
  * Set to the state (HIGH or LOW) that applies to each endstop.
  */
-#define X_MIN_ENDSTOP_HIT_STATE HIGH
+#define X_MIN_ENDSTOP_HIT_STATE LOW
 #define X_MAX_ENDSTOP_HIT_STATE HIGH
 #define Y_MIN_ENDSTOP_HIT_STATE HIGH
 #define Y_MAX_ENDSTOP_HIT_STATE HIGH
