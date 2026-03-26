4deaad2319c55434cf1d30c6469d9816c5315c16
eag1e19 <tdorn6@gmail.com>
2026-03-25 21:56:50 -0400

    Updated location after homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1906,7 +1906,7 @@
                                       // You'll need this much clearance above Z_MAX_POS to avoid grinding.
 
 //#define Z_AFTER_HOMING         10   // (mm) Height to move to after homing (if Z was homed)
-#define XY_AFTER_HOMING { 10, 10 }  // (mm) Move to an XY position after homing (and raising Z)
+#define XY_AFTER_HOMING { 100, 100}  // (mm) Move to an XY position after homing (and raising Z)
 
 //#define EVENT_GCODE_AFTER_HOMING "M300 P440 S200"  // Commands to run after G28 (and move to XY_AFTER_HOMING)
 
